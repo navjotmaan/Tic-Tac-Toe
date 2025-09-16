ef5c91125ce2d90258866f83c190f76b452a428f
navjotmaan <navjotmaan137@gmail.com>
2025-09-16 09:25:23 +0000

    created a module of gameFlow and add the logic to switch players if game is not over yet

--- script.js
+++ script.js
@@ -18,11 +18,31 @@ function createPlayer(name, marker, active) {
     return {
         name: name,
         marker: marker,
-        active: active,
     };
 }
 
-const player1 = createPlayer('player1', 'X', "");
-const player2 = createPlayer('player2', 'O', "");
+const player1 = createPlayer('player1', 'X');
+const player2 = createPlayer('player2', 'O');
 
-const gameFlow = {};
+const gameFlow = (function () {
+    let currentPlayer = player1;
+    let gameOver = false;
+
+    function switchPlayer() {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    function checkWinner() {}
+    
+    return {
+        playRound: (index) => {
+            if (gameOver) return;
+
+            Gameboard.placeMarker(index, currentPlayer.marker);
+
+            if (!gameOver) {
+                switchPlayer();
+            }
+        }
+    }
+})();
