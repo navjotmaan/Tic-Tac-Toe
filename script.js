df618ae96a0b25f9145ea9a2753134c2d900d0c2
navjotmaan <navjotmaan137@gmail.com>
2025-09-16 07:18:20 +0000

    created a module for Gameboard, added an array to track gameboard and methods to change the array state created a factory function for players and a gameflow object

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const Gameboard = {};
+const Gameboard = (function () {
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+    return {
+        placeMarker: (index, marker) => {
+            if (!gameBoard[index]) {
+                gameBoard[index] = marker;
+            }
+        },
+        getBoard: () => {return gameBoard},
+        resetBoard: () => {
+            gameBoard = ["", "", "", "", "", "", "", "", ""];
+        },
+    }
+})();
+
+function createPlayer(name, marker, active) {
+    return {
+        name: name,
+        marker: marker,
+        active: active,
+    };
+}
+
+const player1 = createPlayer('player1', 'X', "");
+const player2 = createPlayer('player2', 'O', "");
+
+const gameFlow = {};
